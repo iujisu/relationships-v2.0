8b5d786e3ddd9d65f714303d7742a13028e9b4fa
iujisu <iujisu@naver.com>
2021-06-14 20:26:34 +0900

    form submit 작업중

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,34 +1,92 @@
-import React from 'react';
-import {  Col, Button, Form, FormGroup, Label, Input,FormText } from 'reactstrap';
-
-
+import React, {useState} from 'react';
+import {  FormFeedback ,Col, Button, Form, FormGroup, Label, Input,FormText } from 'reactstrap';
 
 const Signup = () => {
+
+    const [userName,seUserName] = useState('');
+    const [userId,setUserId] = useState('');
+    const [userPassword,setUserPassword] = useState('');
+    const [rePassword,setRePassword] = useState('');
+    const [term,setTerm] = useState(false);
+    const [passwordError,setPasswordError] = useState(false);
+    const [termError,setTermError] = useState(false);
+
+    const onSubmit = (e) => {
+        console.log("========onsubmit=========");
+        e.preventDefault();
+        /**검증 로직 만들기
+         * 1. 비밀번호와 비밀번호 체크가 다를 경우를 검증한다
+         * 2. 약관 동의를 확인한다.
+         */
+        if(userName === ""){
+            console.log("========name 입력=========");
+            //let element = document.getElementById("userName");
+            //element.setAttribute( 'invalid', false )
+        }
+        if(userPassword !== rePassword){
+            return setPasswordError(true);
+        }
+        if(!term){
+            return setTermError(true);
+        }
+        console.log({
+            userName,
+            userId,
+            userPassword,
+            rePassword,
+            passwordError,
+            termError,
+            term
+        });
+    };
+    const onChangeUserName = (e) => {
+        seUserName(e.target.value);
+    };
+    const onChangeUserId = (e) => {
+        setUserId(e.target.value);
+    };
+    const onChangeUserPassword = (e) => {
+        setUserPassword(e.target.value);
+    };
+    const onChangerePassword = (e) => {
+        //비밀번호를 입력할때마다 password 를 검증하는 함수
+        setPasswordError(e.target.value !== userPassword);
+        setRePassword(e.target.value);
+    };
+    const onChangeTerm = (e) => {
+        //체크박스 초기화
+        setTermError(false);
+        setTerm(e.target.checked);
+    }
+    
+           
+
     return (
         <div >
-             <Form>
-                <FormGroup row>
+             <Form onSubmit={onSubmit}>
+                <FormGroup row className="position-relative">
                     <Label for="userName" sm={3}>name</Label>
                     <Col sm={9}>
-                    <Input type="text" name="userName" id="userName" placeholder="이름" />
+                        <Input type="text" name="userName" id="userName" placeholder="이름" onChange={onChangeUserName}/>
+                        <FormFeedback tooltip>Oh noes! that name is already user id</FormFeedback>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
                     <Label for="userId" sm={3}>id</Label>
                     <Col sm={9}>
-                    <Input type="text" name="userId" id="userId" placeholder="아이디" />
+                    <Input type="text" name="userId" id="userId" placeholder="아이디" onChange={onChangeUserId} />
                     </Col>
                 </FormGroup>
                 <FormGroup row>
                     <Label for="userPassword" sm={3}>Password</Label>
                     <Col sm={9}>
-                    <Input type="password" name="userPassword" id="userPassword" placeholder="비밀번호" />
+                    <Input type="password" name="userPassword" id="userPassword" placeholder="비밀번호" onChange={onChangeUserPassword} />
                     </Col>
                 </FormGroup>
                 <FormGroup row>
                     <Label for="rePassword" sm={3}>rePassword</Label>
                     <Col sm={9}>
-                    <Input type="text" name="rePassword" id="rePassword" placeholder="재확인 비밀번호" />
+                    <Input type="text" name="rePassword" id="rePassword" placeholder="재확인 비밀번호" onChange={onChangerePassword} />
                     </Col>
                 </FormGroup>
                 <FormGroup row>
@@ -53,7 +111,7 @@ const Signup = () => {
                 </FormGroup>
                 <FormGroup check>
                     <Label check>
-                    <Input type="checkbox" />{' '}
+                    <Input type="checkbox" name="user-term" value={term} onChange={onChangeTerm} />{' '}
                         개인정보 이용 확인을 동의함
                     </Label>
                 </FormGroup>
