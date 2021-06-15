772ac08bb872e842c0277db16516e0c8ad2ea9fb
iujisu <iujisu@naver.com>
2021-06-15 18:48:54 +0900

    회원가입 유효성 체크

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -7,24 +7,48 @@ const Signup = () => {
     const [userId,setUserId] = useState('');
     const [userPassword,setUserPassword] = useState('');
     const [rePassword,setRePassword] = useState('');
+    const [phoneNumber,setPhoneNumber] = useState('');
+    const [userEmail,setUserEmail] = useState('');
     const [term,setTerm] = useState(false);
-    const [passwordError,setPasswordError] = useState(false);
+   
     const [termError,setTermError] = useState(false);
+
+    const [nameError,setNameError] = useState(false);
+    const [idError,setIdError] = useState(false);
+    const [passwordError,setPasswordError] = useState(false);
+    const [rePasswordError,setRepasswordError] = useState(false);
+    const [phoneNumberError,setPhnonNumberError] = useState(false);
+    const [emailError,setEmailError] = useState(false);
+
 
     const onSubmit = (e) => {
         console.log("========onsubmit=========");
         e.preventDefault();
-        /**검증 로직 만들기
-         * 1. 비밀번호와 비밀번호 체크가 다를 경우를 검증한다
-         * 2. 약관 동의를 확인한다.
-         */
-        if(userName === ""){
-            console.log("========name 입력=========");
-            //let element = document.getElementById("userName");
-            //element.setAttribute( 'invalid', false )
+
+        setNameError(false);
+        setIdError(false);
+        setPasswordError(false);
+        setRepasswordError(false);
+        setPhnonNumberError(false);
+        setEmailError(false);
+        
+        if(userName === "" ){
+            return setNameError(true);
         }
-        if(userPassword !== rePassword){
+        if(userId === "" ){
+            return setIdError(true);
+        }
+        if(userPassword === "" ){
             return setPasswordError(true);
+        }
+        if(rePassword === "" ){
+            return setRepasswordError(true);
+        }
+        if(phoneNumber === "" ){
+            return setPhnonNumberError(true);
+        }
+        if(userEmail !== rePassword){
+            return setEmailError(true);
         }
         if(!term){
             return setTermError(true);
@@ -40,18 +64,29 @@ const Signup = () => {
         });
     };
     const onChangeUserName = (e) => {
+        setNameError(false);
         seUserName(e.target.value);
     };
     const onChangeUserId = (e) => {
+        setIdError(false);
         setUserId(e.target.value);
     };
     const onChangeUserPassword = (e) => {
+        setPasswordError(false);
         setUserPassword(e.target.value);
     };
     const onChangerePassword = (e) => {
         //비밀번호를 입력할때마다 password 를 검증하는 함수
         setPasswordError(e.target.value !== userPassword);
         setRePassword(e.target.value);
+    };
+    const onChangePhoneNumber = (e) => {
+        setPhnonNumberError(false);
+        setPhoneNumber(e.target.value);
+    };
+    const onChangeEmail = (e) => {
+        setEmailError(false);
+        setUserEmail(e.target.value);
     };
     const onChangeTerm = (e) => {
         //체크박스 초기화
@@ -67,44 +102,49 @@ const Signup = () => {
                 <FormGroup row className="position-relative">
                     <Label for="userName" sm={3}>name</Label>
                     <Col sm={9}>
-                        <Input type="text" name="userName" id="userName" placeholder="이름" onChange={onChangeUserName}/>
-                        <FormFeedback tooltip>Oh noes! that name is already user id</FormFeedback>
+                        <Input type="text" name="userName" id="userName" placeholder="이름" onChange={onChangeUserName} invalid={nameError}/>
+                        <FormFeedback tooltip>이름을 입력해 주세요.</FormFeedback>
                     </Col>
                 </FormGroup>
-                <FormGroup row>
+                <FormGroup row className="position-relative">
                     <Label for="userId" sm={3}>id</Label>
                     <Col sm={9}>
-                    <Input type="text" name="userId" id="userId" placeholder="아이디" onChange={onChangeUserId} />
+                        <Input type="text" name="userId" id="userId" placeholder="아이디" onChange={onChangeUserId} invalid={idError} />
+                        <FormFeedback tooltip>아이디를 입력해 주세요.</FormFeedback>
                     </Col>
                 </FormGroup>
-                <FormGroup row>
+                <FormGroup row className="position-relative">
                     <Label for="userPassword" sm={3}>Password</Label>
                     <Col sm={9}>
-                    <Input type="password" name="userPassword" id="userPassword" placeholder="비밀번호" onChange={onChangeUserPassword} />
+                        <Input type="password" name="userPassword" id="userPassword" placeholder="비밀번호" onChange={onChangeUserPassword} invalid={passwordError}/>
+                        <FormFeedback tooltip>비밀번호를 입력해 주세요.</FormFeedback>
                     </Col>
                 </FormGroup>
-                <FormGroup row>
+                <FormGroup row className="position-relative">
                     <Label for="rePassword" sm={3}>rePassword</Label>
                     <Col sm={9}>
-                    <Input type="text" name="rePassword" id="rePassword" placeholder="재확인 비밀번호" onChange={onChangerePassword} />
+                        <Input type="text" name="rePassword" id="rePassword" placeholder="재확인 비밀번호" onChange={onChangerePassword} invalid={rePasswordError}/>
+                        <FormFeedback tooltip>비밀번호를 확인해 주세요.</FormFeedback>
                     </Col>
                 </FormGroup>
-                <FormGroup row>
+                <FormGroup row className="position-relative">
                     <Label for="phoneNumber" sm={3}>전화번호</Label>
                     <Col sm={9}>
-                    <Input type="text" name="phoneNumber" id="phoneNumber" placeholder="전화번호" />
+                        <Input type="text" name="phoneNumber" id="phoneNumber" placeholder="전화번호" onChange={onChangePhoneNumber} invalid={phoneNumberError}/>
+                        <FormFeedback tooltip>전화번호를 입력해 주세요.</FormFeedback>
                     </Col>
                 </FormGroup>
-                <FormGroup row>
+                <FormGroup row className="position-relative">
                     <Label for="userEmail" sm={3}>Email</Label>
                     <Col sm={9}>
-                    <Input type="email" name="userEmail" id="userEmail" placeholder="이메일" />
+                        <Input type="email" name="userEmail" id="userEmail" placeholder="이메일" onChange={onChangeEmail} invalid={emailError}/>
+                        <FormFeedback tooltip>이메일을 입력해 주세요</FormFeedback>
                     </Col>
                 </FormGroup>
-                <FormGroup row>
+                <FormGroup row className="position-relative">
                     <Label for="exampleFile" sm={3}>증명 이미지</Label>
                     <Col sm={9}>
-                    <Input type="file" name="file" id="exampleFile" />
+                        <Input type="file" name="file" id="userImageFile" />
                     <br/>
                     <FormText color="muted">1024M 미만의 얼굴 이미지 </FormText>
                     </Col>
